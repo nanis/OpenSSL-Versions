11fa9daa92dafa17c8e4526a2c61e517d21e281b
A. Sinan Unur <sinan@archardy.localdomain>
2012-03-08 17:52:45 -0500

    Initial commit of module-starter generated skeleton

---
+++ lib/OpenSSL/Versions.pm
@@ -0,0 +1,111 @@
+package OpenSSL::Versions;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+OpenSSL::Versions - The great new OpenSSL::Versions!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use OpenSSL::Versions;
+
+    my $foo = OpenSSL::Versions->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+A. Sinan Unur, C<< <nanis at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-openssl-versions at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=OpenSSL-Versions>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc OpenSSL::Versions
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=OpenSSL-Versions>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/OpenSSL-Versions>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/OpenSSL-Versions>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/OpenSSL-Versions/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 A. Sinan Unur.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of OpenSSL::Versions
