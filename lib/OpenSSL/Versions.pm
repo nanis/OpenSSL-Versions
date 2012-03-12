51324a230c9d429031e5c0b4b8e17c210bc551f7
A. Sinan Unur <nanis@cpan.org>
2012-03-12 15:39:59 -0400

    First working version

--- lib/OpenSSL/Versions.pm
+++ lib/OpenSSL/Versions.pm
@@ -14,49 +14,120 @@ our @EXPORT = ();
 our @EXPORT_OK = qw( parse_openssl_version_number );
 
 sub parse_openssl_version_number {
-    my ($hex_str) = @_;
-    
-    unless (defined $hex_str) {
-        croak "Must be called with string value of OPENSSL_VERSION_NUMBER as first argument";
+    my ($vstr) = @_;
+
+    $vstr =~ s/^0x//; # remove hex prefix
+
+    # 0.9.6 or later : MNNFFPPS : major, minor, fix, patch, status (0-f)
+    # 0.9.5a to before 0.9.6 : high bit of PP is set in MNNFFPPS
+    # 0.9.3-dev to before 0.9.5a : MNNFFRBB : major, minor, fix, status (0-1), beta
+    # prior to 0.9.3-dev, MNFP
+
+    my @ret;
+
+    if ( $vstr =~ /^(?:[1-9]|0090[6-9])/ ) {
+        @ret = ( OpenSSL => _parse_openssl_v096_or_later( $vstr ) );
+    }
+    elsif ( $vstr =~ /^00905[8-9]/ ) {
+        @ret = ( OpenSSL => _parse_openssl_v095a_096( $vstr ) );
+    }
+    elsif ( $vstr =~ /^0090[3-5][01]/ ) {
+       @ret = ( OpenSSL => _parse_openssl_v093dev_095a( $vstr ) );
+    }
+    else {
+        @ret = ( SSLeay => _parse_openssl_pre_v093dev( $vstr ) );
     }
 
-    if ($hex_str !~ /^0x0/) {
-        return _parse_post_v1_openssl_version_number($hex_str);
-    }
-
-    require OpenSSL::Versions::Table;
-    OpenSSL::Versions::Table->import( 'lookup_openssl_version_number' );
-
-    return lookup_openssl_version_number($hex_str);
+    return wantarray ? @ret : $ret[1];
 }
 
-sub _parse_post_v1_openssl_version_number {
-    my ($hex_str) = @_;
+sub _parse_openssl_v096_or_later {
+    my ($vstr) = @_;
 
-    # from crypto/opensslv.h
-    # MNNFFPPS: major minor fix patch status
-    # The status nibble has one of the values 0 for development, 
-    # 1 to e for betas 1 to 14, 
-    # and f for release. The patch level is exactly that.
-
-    my $xdigit = '[[:xdigit:]]';
-    my $pat = join '', map "(${xdigit}{$_})", (1, 2, 2, 2, 1);
-
-    my @parts = ($hex_str =~ /^0x$pat\z/);
-
-    unless (@parts == 5) {
-        croak "'$hex_str' does not look like a valid OpenSSL v1+ version number";
+    # 0.9.6 or later : MNNFFPPS : major, minor, fix, patch, status (0-f)
+    my $pat = join '', map "([[:xdigit:]]{$_})", (1, 2, 2, 2, 1);
+    
+    my @v = $vstr =~ /^$pat\z/;
+    
+    unless (@v == 5) {
+        _croak_invalid_vstr($vstr);
     }
 
-    my ($major, $minor, $fix, $patch, $status) = map hex, @parts;
+    # The value of OPENSSL_VERSION_NUMBER for in crypto/opensslv.h in the
+    # OpenSSL 0.9.8f distribution is inconsistent with the description. We have
+    # #define OPENSSL_VERSION_NUMBER	0x00908070L in the header file.
+    # However, the comment above the definition states: "The status nibble has
+    # one of the values 0 for development, 1 to e for betas 1 to 14, and f for
+    # release." According to that description, the version string for that
+    # number should be 0.9.8f-dev. This number is special cased below due to
+    # that discrepancy.
+
+    if ($vstr eq '00908070') {
+        return '0.9.8f';
+    }
+
+    my ($major, $minor, $fix, $patch, $status) = map hex, @v;
 
     $patch = $patch ? chr( ord('a') + $patch - 1 ) : '';
 
     if    ( $status ==  0 ) { $status = '-dev' }
     elsif ( $status == 15 ) { $status = '' }
     else                    { $status = "-beta$status" }
-
     return sprintf( '%u.%u.%u%s%s', $major, $minor, $fix, $patch, $status);
+}
+
+sub _parse_openssl_v095a_096 {
+    my ($vstr) = @_;
+
+    # 0.9.5a to before 0.9.6 :
+    # high bit of PP is set in MNNFFPPS
+
+    return _parse_openssl_v096_or_later(
+        sprintf '%08x', hex($vstr) & 0xfffff7ff
+    );
+}
+
+sub _parse_openssl_v093dev_095a {
+    my ($vstr) = @_;
+
+    # Prior to 0.9.5a beta1, a different scheme was used: 
+    # MMNNFFRBB for major minor fix final patch/beta)
+
+    my @v = $vstr =~ /^(0)(09)(0[3-5])([0-1])(..)\z/;
+
+    unless (@v == 5) {
+        _croak_invalid_vstr($vstr);
+    }
+
+    my ($major, $minor, $fix, $status, $patch) = map hex, @v;
+
+    $patch = $patch ? chr( ord('a') + $patch - 1 ) : '';
+    $status = $status ? '' : '-dev';
+    return sprintf( '%u.%u.%u%s%s', $major, $minor, $fix, $patch, $status );
+}
+
+sub _parse_openssl_pre_v093dev {
+    my ($vstr) = @_;
+
+    # prior to 0.9.3-dev,
+    # MNFP : major, minor, fix, patch
+
+    my @v = $vstr =~ /^(0)(9)([12])([0-9])$/;
+
+    unless (@v == 4) {
+        croak "'$vstr' does not look like a valid value for OPENSSL_VERSION_NUMBER";
+    }
+
+    my ($major, $minor, $fix, $patch) = map hex, @v;
+
+    $patch = $patch ? chr( ord('a') + $patch - 1 ) : '';
+    return sprintf( '%u.%u.%u%s', $major, $minor, $fix, $patch );
+}
+
+sub _croak_invalid_vstr {
+    my ($vstr) = @_;
+    
+    croak "'$vstr' does not look like a valid value for OPENSSL_VERSION_NUMBER";
 }
 
 1;
@@ -96,11 +167,9 @@ versus
 
     #define OPENSSL_VERSION_NUMBER	0x1000007fL /* OpenSSL 1.0.0g */
 
-
 The evolution of the version number scheme is explained in the
-C<crypto/opensslv.h> file in recent distributions. If you have a system wide
-default OpenSSL library, and you want to determine its version, that is as
-simple as invoking a command line utility:
+C<crypto/opensslv.h> file in the distribution. If you have already built
+OpenSSL, you can determine its version by invoking the command line utility:
 
     $ openssl version
     OpenSSL 1.0.0g 18 Jan 2012
@@ -108,40 +177,29 @@ simple as invoking a command line utility:
 However, if all you have is the source code, and you want to determine exact
 version information on the basis of the string representation of the
 OPENSSL_VERSION_NUMBER macro, you have to use pattern matching and deal with a
-bunch of corner cases. The post v.1.0.0 case is easy, but there are various
-breakpoints in the release history. Any routine you write must be tested and
-verified against the existing distributions.
+bunch of corner cases. 
 
-I had inserted such a routine in the C<Makefile.PL> for L<Crypt::SSLeay>. That
-routine was not formally tested. I had just thrown a bunch of version numbers
-at it and tinkered with it until it seemed to produce the appropriate strings
-in response.
+The C<Makefile.PL> for L<Crypt::SSLeay> contained a simplistic approach to
+parsing the value of OPENSSL_VERSION_NUMBER which people had tweaked over time
+to deal with changes. I added functions to deal with specific ranges of version
+numbers. But, I did not think those functions belonged in a C<Makefile.PL>.
 
-So, I decided to write proper tests for it. To write the tests, I needed to
-make a table of all values for OPENSSL_VERSION_NUMBER in the source code for
-all available OpenSSL distributions and the corresponding version strings. I
-fired up C<wget>, downloaded all the archives from
-L<http://www.openssl.org/source/> and wrote a quick parsing script which simply
-associated with the value of the macro OPENSSL_VERSION_NUMBER the name of the
-C<.tar.gz> file in which it was found.
-
-So, I was ready to write the tests, right?
-
-Well, what was the point of hiding in a test file the information I had just
-extracted? So, I decided to put together a module to distribute the
-information. However, I did not want to have to update the module every time a
-new version of the OpenSSL library was made available.
-
-Given that the format seems to be much more stable starting with 1.0.0, the
-C<parse_openssl_version> function provided by this module checks if the string
-it was passed starts with the digit 1. If so, it simply parses the hexadecimal
-string using pattern matching. Otherwise, it looks up the human-friendly
-version string in a table.
+So, I put them in their own module. To test the routines, I downloaded all
+available versions of OpenSSL from http://www.openssl.org/source/ (excluding
+archives with 'fips' and 'engine' in their names, and built a mapping between
+the value of OPENSSL_VERSION_NUMBER in each archive and the corresponding human
+friendly version string in the name of the archive.
 
 =head1 EXPORT
 
+=over 4
+
+=item C<parse_openssl_version_number>
+
 By default, this module does not export anything. However, you can ask for
 C<parse_openssl_version_number> to be exported.
+
+=back
 
 =head1 SUBROUTINES
 
@@ -149,8 +207,11 @@ C<parse_openssl_version_number> to be exported.
 
 Takes a hexadecimal string corresponding to the value
 of the macro C<OPENSSL_VERSION_NUMBER> macro in either C<crypto.h> or
-C<openssl/opensslv.h> file in an OpenSSL distribution and returns a human
-friendly version string such as '0.9.8q'.
+C<openssl/opensslv.h> file in an OpenSSL distribution.
+
+In scalar context returns a human friendly version string such as '0.9.8q'. In
+list context, it returns a pair of values the first of which is the human
+friendly version string and the second is either 'OpenSSL' or 'SSLeay'.
 
 =head1 AUTHOR
 
